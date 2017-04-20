0aba6553a6fe4920c05a1132af0305375533dd45
Тестируемый(ая) <t@local.rsu.edu.ru>
2017-04-20 19:32:32 +0300

    complite task

--- task1.js
+++ task1.js
@@ -1,24 +1,26 @@
 
 // Declare two variables and show them
-console.log("Declare two variables and show them");
-const a = 5;
-const b = 7;
-console.log(a + " " + b);
+console.log("1. Declare two variables and show them");
+var variableOne = 5;
+var variableTwo = 7;
+console.log(variableOne + " " + variableTwo);
 
 
 // Write a function that can find triangle's area
-console.log("Write a function that can find triangle's area");
-const functionTriangleArea = function triangleArea(a, b, c) {
-    var p = (a + b + c) / 2;
-    return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+console.log("2. Write a function that can find triangle's area");
+const functionTriangleArea = function findTriangleArea (a, b, c) {
+    const halfPerimetr = (a + b + c) / 2;    
+    return Math.sqrt(halfPerimetr * (halfPerimetr - a) * (halfPerimetr - b) * (halfPerimetr - c));;
 }
-const c = 10;
-console.log("Triangle area with " + "a=" + a + ", " + "b=" + b + ", " + "c=" + c + ", area=" + functionTriangleArea(a, b, c));
+const triangeleSideA = 5;
+const triangeleSideB = 7;
+const triangeleSideC = 10;
+console.log("Triangle area with sides" + "a=" + triangeleSideA + ", " + "b=" + triangeleSideB + ", " + "c=" + triangeleSideC + ", area=" + functionTriangleArea(triangeleSideA, triangeleSideB, triangeleSideC));
 
 
 // Write a function that reverse the given array using three kind of loops
-console.log("Write a function that reverse the given array using three kind of loops");
-const d = [1, 2, 3, 4, 5, 6];
+console.log("3. Write a function that reverse the given array using three kind of loops");
+const someArr = [1, 2, 3, 4, 5, 6];
 
 const functionReverseArrFor = function ReverseArrFor(arr) {
     var i;
@@ -55,23 +57,23 @@ const functionReverseArrDowhile = function ReverseArrDowhile(arr) {
     return arr;
 }
 
-console.log("Inintial array " + d);
-console.log("Reversed array with for loop " + functionReverseArrFor(d));
-console.log("Inintial array " + d);
-console.log("Reversed array with while loop " + functionReverseArrWhile(d));
-console.log("Inintial array " + d);
-console.log("Reversed array with dowhile loop " + functionReverseArrDowhile(d));
+console.log("Inintial array [" + someArr + "]");
+console.log("Reversed array with \"for\" loop [" + functionReverseArrFor(someArr) + "]");
+console.log("Inintial array [" + someArr + "]");
+console.log("Reversed array with \"while\" loop [" + functionReverseArrWhile(someArr) + "]");
+console.log("Inintial array [" + someArr + "]");
+console.log("Reversed array with \"dowhile\" loop [" + functionReverseArrDowhile(someArr) + "]");
 
 
 //Differnce between ++i i++
-console.log("Differnce between ++i i++ is that first one first of all increase value and then return increased value i, and second one firstly return value i and only then increase");
-var i = 0,
-    j;
+console.log("4. Differnce between ++i i++ is that first one first of all increase value and then return increased value i, and second one firstly return value i and only then increase");
+var i = 0;
+var j = 0;
 j = i++;
-console.log("j = i++" + " | j = " + j + " i = " + i);
+console.log("j = i++" + " where j = " + j + ", i = " + i);
 j = ++i;
-console.log("j = ++i" + " | j = " + j + " i = " + i);
-console.log("------------");
+console.log("j = ++i" + " where j = " + j + ", i = " + i);
+
 
 //Function that checks if the given arguments is positive number or negative number or is 0
 const functionCheckNumber = function checkNumber(arg) {
@@ -94,15 +96,16 @@ console.log("0 is", functionCheckNumber(0));
 console.log("1 is", functionCheckNumber(1));
 console.log("-1 is", functionCheckNumber(-1));
 console.log("'a' is", functionCheckNumber("a"));
-console.log("------------");
+
 
 // Asking the user his name and alert it back
-/*const user_name = prompt("Hello! What is your name?");
-console.log("Nice to meet you, " + user_name);*/
-console.log("------------");
+console.log("5. Asking the user his name and alert it back");
+let user_name = prompt("Hello! What is your name?");
+console.log("Nice to meet you, " + user_name);
 
 // Calculation fatorial
-function factorial(n) {
+console.log("6. Calculation fatorial");
+const functionFactorial = function factorial(n) {
     var f = 1;
     var i;
     for (i = 2; i <= n; i++) {
@@ -110,14 +113,5 @@ function factorial(n) {
     }
     return f;
 }
-
-var f = 5;
-console.log("Factorial of " + f + " = " + factorial(f));
-/* */
-var obj = {"key1":"value1"};
-function myFunction(x) {
-    console.log(x); // undefined
-}
-myFunction(...obj);
-var args = [...obj];
-console.log(args, args.length)
+const calcFactorial = 5;
+console.log("Factorial of " + calcFactorial + " is" + functionFactorial(calcFactorial));
